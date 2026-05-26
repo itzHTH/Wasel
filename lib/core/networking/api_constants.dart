fb73635d8f602065924f51e8295affe05cb72288
Huthaifa Mohammed <jhuth981@gmail.com>
2026-05-26 15:20:50 +0300

    feat: update API endpoints and base URLs for development and production environments

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -6,7 +6,15 @@ class ApiConstants {
   static const String acceptEncoding = 'br';
 
   // Auth
-  static const String login = '/auth/login';
+  static const String login = '/api/v1/Auth/login';
+  static const String initiateRegistration =
+      '/api/v1/Auth/rider/initiate-registration';
+  static const String verifyRegistration =
+      '/api/v1/Auth/rider/verify-registration';
+  static const String refreshToken = '/api/v1/Auth/refresh-token';
+  static const String revokeToken = '/api/v1/Auth/revoke-token';
+  static const String forgotPassword = '/api/v1/Auth/forgot-password';
+  static const String resetPassword = '/api/v1/Auth/reset-password';
 
   //
 }
