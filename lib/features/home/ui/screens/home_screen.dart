e36e2c1fb901d18f0af55b93acb58e3c0cea46fb
Huthaifa Mohammed <jhuth981@gmail.com>
2026-06-16 00:31:03 +0300

    feat:  Implement  authentication check and logout functionality

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wasal/core/helpers/app_local_cache.dart';
+import 'package:wasal/core/routing/app_routes_name.dart';
+import 'package:wasal/core/theme/app_color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +10,34 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('الرئيسية')),
-      body: const Center(child: Text('مرحباً بك في تطبيقنا!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('مرحباً بك في تطبيقنا!', style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColor.alertError500,
+                foregroundColor: AppColor.neutral200,
+                fixedSize: const Size(250, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+
+              onPressed: () {
+                AppLocalCache.clearAllSecuredData().then((_) {
+                  if (context.mounted) {
+                    Navigator.pushReplacementNamed(context, AppRoutes.auth);
+                  }
+                });
+              },
+              child: const Text("تسجيل الخروج"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/features/splash/ui/screens/splash_screen.dart
+++ lib/features/splash/ui/screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:wasal/core/const/app_constants.dart';
 import 'package:wasal/core/extensions/navigation_extension.dart';
+import 'package:wasal/core/helpers/app_local_cache.dart';
 import 'package:wasal/core/routing/app_routes_name.dart';
 import 'package:wasal/core/theme/app_color.dart';
 import 'package:wasal/core/theme/app_text_styles.dart';
@@ -61,10 +63,25 @@ class _SplashScreenState extends State<SplashScreen>
     _controller
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          context.pushNamed(AppRoutes.auth);
+          handleIsAuthenticatedUser();
         }
       })
       ..forward();
+  }
+
+  void handleIsAuthenticatedUser() async {
+    final token = await AppLocalCache.getSecuredString(AppConstants.tokenKey);
+    final refreshToken = await AppLocalCache.getSecuredString(
+      AppConstants.refreshTokenKey,
+    );
+
+    if (token != null && refreshToken != null) {
+      if (!mounted) return;
+      Navigator.pushReplacementNamed(context, AppRoutes.home);
+    } else {
+      if (!mounted) return;
+      context.pushReplacementNamed(AppRoutes.auth);
+    }
   }
 
   @override
