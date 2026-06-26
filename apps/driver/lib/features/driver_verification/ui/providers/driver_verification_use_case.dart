d8adebbd324b25665416217f6d7542dccaf4a547
Huthaifa Mohammed <jhuth981@gmail.com>
2026-06-27 02:19:15 +0300

    feat(driver): add driver verification use case and status provider

---
+++ apps/driver/lib/features/driver_verification/ui/providers/driver_verification_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:driver/features/driver_verification/data/repo/verify_repo.dart';
+import 'package:driver/features/driver_verification/domain/usecases/get_verification_status_use_case.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'driver_verification_use_case.g.dart';
+
+@riverpod
+GetVerificationStatusUseCase getVerificationStatusUseCase(Ref ref) {
+  final repo = ref.watch(verifyRepoProvider);
+  return GetVerificationStatusUseCase(repo);
+}
