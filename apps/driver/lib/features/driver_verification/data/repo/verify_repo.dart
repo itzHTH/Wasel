3e4bd5442cf9d3937dfc00bda5a56e72c17089c0
Huthaifa Mohammed <jhuth981@gmail.com>
2026-06-27 01:56:57 +0300

    feat(driver): implement Riverpod providers for VerifyRepo and VerifyApiService

--- apps/driver/lib/features/driver_verification/data/repo/verify_repo.dart
+++ apps/driver/lib/features/driver_verification/data/repo/verify_repo.dart
@@ -1,8 +1,11 @@
 import 'package:driver/features/driver_verification/data/services/verify_api_service.dart';
 import 'package:driver/features/driver_verification/domain/entities/verification_status.dart';
 import 'package:driver/features/driver_verification/domain/repo/base_verify_repo.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:wasel_core/networking/api_results.dart';
 import 'package:wasel_core/networking/errors/error_handler.dart';
+
+part 'verify_repo.g.dart';
 
 class VerifyRepo implements BaseVerifyRepo {
   VerifyApiService apiService;
@@ -18,4 +21,10 @@ class VerifyRepo implements BaseVerifyRepo {
       return ApiResults.failure(ErrorHandler.handle(e));
     }
   }
+}
+
+@riverpod
+VerifyRepo verifyRepo(Ref ref) {
+  final verifyApiService = ref.watch(verifyApiServiceProvider);
+  return VerifyRepo(verifyApiService);
 }
--- apps/driver/lib/features/driver_verification/data/services/verify_api_service.dart
+++ apps/driver/lib/features/driver_verification/data/services/verify_api_service.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:driver/core/const/driver_api_consts.dart';
 import 'package:driver/features/driver_verification/data/models/get_verification_status/response/get_verification_status_response.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:wasel_core/wasel_core.dart';
 
 part 'verify_api_service.g.dart';
 
@@ -14,4 +16,10 @@ abstract class VerifyApiService {
   Future<GetVerificationStatusResponse> getVerificationStatus({
     @CancelRequest() CancelToken? cancelToken,
   });
+}
+
+@riverpod
+VerifyApiService verifyApiService(Ref ref) {
+  final dio = ref.watch(dioFactoryProvider);
+  return VerifyApiService(dio, baseUrl: ApiConstants.baseUrl);
 }
