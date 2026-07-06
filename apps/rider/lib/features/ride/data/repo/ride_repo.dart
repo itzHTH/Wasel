a1892cee64945f5092ce5ab93ac7467b5f12a2b2
Huthaifa Mohammed <jhuth981@gmail.com>
2026-07-07 01:52:14 +0300

    feat(ride): add ride price estimation data and domain layers

---
+++ apps/rider/lib/features/ride/data/repo/ride_repo.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:wasal/features/ride/data/models/estimate_ride_price/response/estimate_ride_price_response.dart';
+import 'package:wasal/features/ride/data/models/geo_point_request/geo_point_request_body.dart';
+import 'package:wasal/features/ride/data/services/ride_api_service.dart';
+import 'package:wasal/features/ride/domain/entities/ride_price.dart';
+import 'package:wasal/features/ride/domain/repo/base_ride_repo.dart';
+import 'package:wasel_core/wasel_core.dart';
+
+part 'ride_repo.g.dart';
+
+class RideRepo extends BaseRideRepo {
+  final RideApiService _rideApiService;
+
+  RideRepo(this._rideApiService);
+
+  @override
+  Future<ApiResults<RidePrice>> estimateRidePrice(
+    GeoPointRequestBody geoPointRequest, {
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      final EstimateRidePriceResponse response = await _rideApiService
+          .estimateRidePrice(geoPointRequest, cancelToken: cancelToken);
+
+      return ApiResults.success(response.toEntity());
+    } catch (e) {
+      return ApiResults.failure(ErrorHandler.handle(e));
+    }
+  }
+}
+
+@riverpod
+BaseRideRepo rideRepo(Ref ref) {
+  final rideApiService = ref.watch(rideApiServiceProvider);
+  return RideRepo(rideApiService);
+}
--- apps/rider/lib/features/ride/ui/providers/ride_use_case_providers.dart
+++ apps/rider/lib/features/ride/ui/providers/ride_use_case_providers.dart
@@ -1,7 +1,9 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:wasal/features/ride/data/repo/fake_geocoding_repo.dart';
+import 'package:wasal/features/ride/data/repo/ride_repo.dart';
 import 'package:wasal/features/ride/domain/repo/base_geocoding_repo.dart';
 import 'package:wasal/features/ride/domain/usecases/get_point_label_use_case.dart';
+import 'package:wasal/features/ride/domain/usecases/get_ride_price_use_case.dart';
 
 part 'ride_use_case_providers.g.dart';
 
@@ -12,4 +14,10 @@ BaseGeocodingRepo geocodingRepo(Ref ref) => FakeGeocodingRepo();
 GetPointLabelUseCase getPointLabelUseCase(Ref ref) {
   final geocodingRepo = ref.watch(geocodingRepoProvider);
   return GetPointLabelUseCase(geocodingRepo);
+}
+
+@riverpod
+GetRidePriceUseCase getRidePriceUseCase(Ref ref) {
+  final rideRepo = ref.watch(rideRepoProvider);
+  return GetRidePriceUseCase(rideRepo);
 }
