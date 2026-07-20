98fad07e88a8b4e1607a596e45cc0ce3b4134fba
Huthaifa Mohammmed <jhuth981@gmail.com>
2026-07-21 00:29:45 +0300

    Implement Flavors in IOS for Rider App, and first run In IOS Device .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/leader/fvm/versions/3.44.0"
-export "FLUTTER_APPLICATION_PATH=/home/leader/Flutter Projects/wasel"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/home/leader/Flutter Projects/wasel/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
+export "FLUTTER_ROOT=/Users/leader/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/leader/Development/Projects/wasel"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/leader/Development/Projects/wasel/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
